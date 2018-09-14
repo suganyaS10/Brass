f7f226a18c5b8eaa6ca8832c8c71eb153a289dee
suganyaS10 <sugukvs92@gmail.com>
2018-09-14 14:35:07 +0530

    Added user Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_products
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   def set_products
   	@products = Product.all
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id])
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,22 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.smtp_settings =
+   { 
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :authentication => :plain,
+     :domain => 'gmail.com',
+     :user_name => 'sugukvs92@gmail.com',
+     :password => 'Selvarajkv10',
+   }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
